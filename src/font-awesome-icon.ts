d55d1591b3f26bb3f3bfbe0ebad6a503af67d164
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2019-02-11 09:13:32 +0100

    fix(customElement) fix case where properties are bound to undefined

--- src/font-awesome-icon.ts
+++ src/font-awesome-icon.ts
@@ -118,14 +118,14 @@ export class FontAwesomeIconCustomElement {
     this.overrideContext = createOverrideContext(bindingContext, overrideContext);
 
     this.classes = {
-      'fa-border': this.border.toString() === 'true',
+      'fa-border': this.border && this.border.toString() === 'true',
       'fa-flip-horizontal': this.flip === 'horizontal' || this.flip === 'both',
       'fa-flip-vertical': this.flip === 'vertical' || this.flip === 'both',
-      'fa-fw': this.fixedWidth.toString() === 'true',
-      'fa-inverse': this.inverse.toString() === 'true',
-      'fa-li': this.listItem.toString() === 'true',
-      'fa-pulse': this.pulse.toString() === 'true',
-      'fa-spin': this.spin.toString() === 'true',
+      'fa-fw': this.fixedWidth && this.fixedWidth.toString() === 'true',
+      'fa-inverse': this.inverse && this.inverse.toString() === 'true',
+      'fa-li': this.listItem && this.listItem.toString() === 'true',
+      'fa-pulse': this.pulse && this.pulse.toString() === 'true',
+      'fa-spin': this.spin && this.spin.toString() === 'true',
       [`fa-${this.size}`]: !!this.size,
       [`fa-pull-${this.pull}`]: !!this.pull,
       [`fa-rotate-${this.rotation}`]: !!this.rotation
