f28e48599bd7750b8582e2aebbaa3d132393c4e8
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2019-02-14 13:59:50 +0100

    Revert "fix(customElement) ensure that the view is safely removed"
    This reverts commit c2ce0f3f563ad8ea8c481f727a0fd008c9df8254.

--- src/font-awesome-icon.ts
+++ src/font-awesome-icon.ts
@@ -259,9 +259,7 @@ export class FontAwesomeIconCustomElement {
     return {
       $icon,
       dispose: () => {
-        // It may be that the view is already removed from the slot,
-        // e.g. if the element has an if.bind
-        slot.removeAll();
+        slot.remove(view);
         view.unbind();
       }
     }
